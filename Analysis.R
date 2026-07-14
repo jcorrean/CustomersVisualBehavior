1de77fe18c6ffe95947b5dc82586a2af9f68939a
Juan C. Correa <j.correa.n@gmail.com>
2026-07-14 11:30:25 -0600

    Update Analysis.R

--- Analysis.R
+++ Analysis.R
@@ -81,17 +81,171 @@ ggplot(
   geom_col(fill="darkgreen")+
   coord_flip()
 
-all_edges <- bind_rows(
-  lapply(
-    NetworkList,
-    function(x) x$edges
+
+#########################################################
+# RQ2
+#########################################################
+
+tabla_firstAOI <- table(
+  TrajectoryFeatures$FirstAOI,
+  TrajectoryFeatures$Choice
+)
+
+test_firstAOI <- chisq.test(
+  tabla_firstAOI
+)
+
+tabla_firstTransition <- table(
+  TrajectoryFeatures$FirstTransition,
+  TrajectoryFeatures$Choice
+)
+
+test_firstTransition <- chisq.test(
+  tabla_firstTransition
+)
+
+tabla_first3 <- table(
+  TrajectoryFeatures$First3AOIs,
+  TrajectoryFeatures$Choice
+)
+
+test_first3 <- chisq.test(
+  tabla_first3
+)
+
+anova_nodes <- summary(
+  aov(
+    Nodes ~ Choice,
+    data = TrajectoryFeatures
   )
 )
 
-agg_edges <- all_edges %>%
-  count(from,to,name="weight")
+anova_edges <- summary(
+  aov(
+    Edges ~ Choice,
+    data = TrajectoryFeatures
+  )
+)
 
-mat <- xtabs(
-  weight ~ from + to,
-  data = agg_edges
+anova_entropy <- summary(
+  aov(
+    Entropy ~ Choice,
+    data = TrajectoryFeatures
+  )
+)
+
+multiverse_results <- data.frame(
+  Universe = c(
+    "FirstAOI",
+    "FirstTransition",
+    "First3AOIs",
+    "Nodes",
+    "Edges",
+    "Entropy"
+  ),
+  Test = c(
+    "Chi-square",
+    "Chi-square",
+    "Chi-square",
+    "ANOVA",
+    "ANOVA",
+    "ANOVA"
+  ),
+  Pvalue = c(
+    test_firstAOI$p.value,
+    test_firstTransition$p.value,
+    test_first3$p.value,
+    anova_nodes[[1]]["Choice","Pr(>F)"],
+    anova_edges[[1]]["Choice","Pr(>F)"],
+    anova_entropy[[1]]["Choice","Pr(>F)"]
+  )
+)
+
+knitr::kable(
+  multiverse_results,
+  digits = 4,
+  caption = "Multiverse analysis of visual exploration."
+)
+
+library(nnet)
+library(pscl)
+library(knitr)
+library(dplyr)
+
+# Asegurar que es factor
+TrajectoryFeatures$Choice <- as.factor(
+  TrajectoryFeatures$Choice
+)
+
+#################################################
+# MODELS
+#################################################
+
+m1 <- multinom(
+  Choice ~ Entropy,
+  data = TrajectoryFeatures,
+  trace = FALSE
+)
+
+m2 <- multinom(
+  Choice ~ Entropy + FirstAOI,
+  data = TrajectoryFeatures,
+  trace = FALSE
+)
+
+m3 <- multinom(
+  Choice ~ Entropy + FirstTransition,
+  data = TrajectoryFeatures,
+  trace = FALSE
+)
+
+m4 <- multinom(
+  Choice ~ Nodes + Edges + Entropy,
+  data = TrajectoryFeatures,
+  trace = FALSE
+)
+
+#################################################
+# TABLE
+#################################################
+
+results_models <- data.frame(
+  Model = c(
+    "M1",
+    "M2",
+    "M3",
+    "M4"
+  ),
+  
+  Predictors = c(
+    "Entropy",
+    "FirstAOI + Entropy",
+    "FirstTransition + Entropy",
+    "Nodes + Edges + Entropy"
+  ),
+  
+  McFadden_R2 = c(
+    pR2(m1)["McFadden"],
+    pR2(m2)["McFadden"],
+    pR2(m3)["McFadden"],
+    pR2(m4)["McFadden"]
+  ),
+  
+  AIC = c(
+    AIC(m1),
+    AIC(m2),
+    AIC(m3),
+    AIC(m4)
+  )
+)
+
+results_models <- results_models %>%
+  mutate(
+    McFadden_R2 = round(McFadden_R2, 4),
+    AIC = round(AIC, 1)
+  )
+
+kable(
+  results_models,
+  caption = "Comparative performance of multinomial models predicting booking decisions from alternative representations of visual exploration."
 )
